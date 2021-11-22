e35305675b8d50022b218ab83967e06969e600f4
minherz <minherz@gmail.com>
2021-11-22 13:45:39 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+project_id=${1:-`gcloud config get-value core/project`}
+if [ -z "${project_id}" ]
+then
+    echo "Pass project id as argument or set project using 'gcloud config set project'"
+    exit 1
+fi
+
+echo "🏗️ Enabling Google APIs..."
+gcloud --project ${project_id} services enable \
+    compute.googleapis.com \
+    pubsub.googleapis.com \
+    dataflow.googleapis.com \
+    dlp.googleapis.com
+
+echo "📟 Preparing PubSub..."
+pubsub_topic=`gcloud --project ${project_id} pubsub topics describe log_ingestion_topic --format="value(name)"`
+if [ -z "$pubsub_topic" ]
+then
+    gcloud --project=${project_id} pubsub topics create log_ingestion_topic
+    gcloud --project=${project_id} pubsub subscriptions create log_ingestion_subscr --topic=log_ingestion_topic
+fi
+
+echo "🚰 Configuring export sink..."
+export_sink_sa=`gcloud --project ${project_id} logging sinks describe log-with-pii-sink --format="value(writerIdentity)"`
+if [ -z "$export_sink_sa" ]
+then
+    pubsub_topic="projects/${project_id}/topics/log_ingestion_topic"
+    logname="projects/${project_id}/logs/logs-with-pii" 
+    gcloud --project=${project_id} logging sinks create log-with-pii-sink "pubsub.googleapis.com/${pubsub_topic}" \
+        --log-filter="logName=${logname}"
+    cat > policy.yml <<EOF
+bindings:
+- members:
+    - ${export_sink_sa}
+    role: roles/pubsub.publisher
+EOF
+    gcloud --project=${project_id} pubsub topics set-iam-policy log_ingestion_topic policy.yaml --quiet
+fi
+
+echo "🔦 Deploy Dataflow pipeline..."
+bucket_name="gs://${project_id}-example-bucket"
+gsutil list ${bucket_name}
+if [ $? == 1 ]
+then
+    gsutil mb ${bucket_name}
+fi
+pip3 install wheel
+pip3 install apache_beam[gcp]
+pip3 install google-cloud-blp google-cloud-logging --upgrade
+python3 -m deidentify_logs --runner DataflowRunner --region us-central1 --temp_location "${bucket_name}/temp" \
+--project ${project_id} --input log_ingestion_subscr --output_logname no-pii-logs
